abaea3eb759998a68be4c8514caf6527f35ceaae
radhamadhab23 <radhamadhabpattnaik23@gmail.com>
2025-07-08 22:04:09 +0530

    Added Flight model with foreign key constraint defined during model definition ``` npx sequelize-cli model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,departureTime:date,arrivalTime:date,price:integer,boardingGate:string,totalSeats:integer ```

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -9,18 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   // define association here
-    //   this.hasMany(models.Flight, {
-    //     foreignKey: 'airplaneId',
-    //     onDelete: 'CASCADE'
-    //   });
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Flight, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      });
     //   this.hasMany(models.Seat, {
     //     foreignKey: 'airplaneId',
     //     onDelete: 'CASCADE'
     //   });
     // }
   }
+}
   Airplane.init({
     modelNumber: {
       type: DataTypes.STRING,
--- src/models/airport.js
+++ src/models/airport.js
@@ -14,6 +14,14 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
         as: 'city'
       });
+       this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId',
+        onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airport.init({
